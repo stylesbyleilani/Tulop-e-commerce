8a256e65e3c2505cc0b6c45bf54b8a7e75f51e99
mustapha adeola <mustaphaadeola948@gmail.com>
2024-12-18 04:14:50 +0100

    made some changes in the conig

--- src/Lib/firebase.js
+++ src/Lib/firebase.js
@@ -9,12 +9,15 @@ import { getStorage } from "firebase/storage";
 
 import { getFirestore, initializeFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDoXJsjVAXfBh5-h3sUDgBvO8emlD7Yd7s",
+  apiKey: import.meta.env.REACT_APP_API_KEY,
+  apiKey:"AIzaSyDoXJsjVAXfBh5-h3sUDgBvO8emlD7Yd7s",
   authDomain: "gadget-store-4fc83.firebaseapp.com",
   projectId: "gadget-store-4fc83",
   storageBucket: "gadget-store-4fc83.firebasestorage.app",
   messagingSenderId: "291550916611",
   appId: "1:291550916611:web:4dd9988a2ffab956159e36"
+  
+
 };
 
 
